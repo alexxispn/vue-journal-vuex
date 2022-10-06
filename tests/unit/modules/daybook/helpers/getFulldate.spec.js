7f34bc4a2b3919d9beb9c0ae447d92b6bc5e98c5
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-06 15:35:05 +0100

    update test getFullDate

--- tests/unit/modules/daybook/helpers/getFulldate.spec.js
+++ tests/unit/modules/daybook/helpers/getFulldate.spec.js
@@ -1,7 +1,7 @@
 import getFullDate from "@/modules/daybook/helpers/getFullDate";
 
 describe("getFullDate helper", () => {
-    it("should return an object with the date, month, year and weekday", () => {
+    it("should return an object with the date, month, year, weekday and fullDate", () => {
         const date = new Date("2022-10-04");
         const fullDate = getFullDate(date);
         expect(fullDate).toEqual({

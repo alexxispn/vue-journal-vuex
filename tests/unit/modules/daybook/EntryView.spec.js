5ccd587b287d8312c4ea8be2da5d900878fcc8c9
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-07 12:49:29 +0100

    add tests to the view EntryView

--- tests/unit/modules/daybook/EntryView.spec.js
+++ tests/unit/modules/daybook/EntryView.spec.js
@@ -1,5 +1,6 @@
 import {shallowMount} from "@vue/test-utils";
 import {createStore} from "vuex";
+import Swal from "sweetalert2";
 import journal from "@/modules/daybook/store/journal";
 import {journalState} from "../../mock-data/journal-state";
 import EntryView from "@/modules/daybook/views/EntryView";
@@ -13,9 +14,7 @@ const createVuexStore = (state) => createStore({
     }
 });
 jest.mock('sweetalert2', () => ({
-    fire: jest.fn(),
-    showLoading: jest.fn(),
-    close: jest.fn()
+    fire: jest.fn()
 }))
 
 describe('EntryView component', () => {
@@ -42,7 +41,7 @@ describe('EntryView component', () => {
     it('should match snapshot', () => {
         expect(wrapper.html()).toMatchSnapshot();
     });
-    it('should redirect to /daybook if entry does not exist', () => {
+    it('should redirect to /no-entry if entry does not exist', () => {
         wrapper = shallowMount(EntryView, {
             props: {
                 id: '123'
@@ -55,6 +54,15 @@ describe('EntryView component', () => {
             }
         });
         expect(mockRouter.push).toHaveBeenCalledWith({name: 'no-entry'});
+    });
+    it('should delete entry and redirect to /no-entry', (done) => {
+        Swal.fire.mockResolvedValueOnce({isConfirmed: true});
+        wrapper.find('.btn-outline-danger').trigger('click');
+        setTimeout(() => {
+            expect(store.dispatch).toHaveBeenCalledWith('journal/deleteEntry', '-NDTTxqbPCxX382b6UBi');
+            expect(mockRouter.push).toHaveBeenCalledWith({name: 'no-entry'});
+            done();
+        }, 1000);
     });
 });
 
